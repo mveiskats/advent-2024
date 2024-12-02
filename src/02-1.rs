bdb0f2eba12b953e27beb271d63cba6d13111d68
Martins Veiskats <martins.veiskats@gmail.com>
2024-12-02 09:04:29 +0200

    Day 2 solution

---
+++ src/02-1.rs
@@ -0,0 +1,20 @@
+use std::io;
+use itertools::Itertools;
+
+fn main() {
+    let safe: usize = io::stdin().lines()
+        .map(|line| line.expect("read error"))
+        .map(|line| {
+            line.split(' ')
+                .map(|level| level.parse::<usize>().expect("not a number"))
+                .collect::<Vec<_>>()
+        })
+        .filter(|levels|{
+            levels.iter().tuple_windows().all(|(&l1, &l2)| l1.abs_diff(l2) <= 3) &&
+                (levels.iter().tuple_windows().all(|(l1, l2)| l1 < l2) ||
+                 levels.iter().tuple_windows().all(|(l1, l2)| l1 > l2))
+        })
+        .count();
+
+    println!("{safe}")
+}
---
+++ src/02-2.rs
@@ -0,0 +1,29 @@
+use std::io;
+use itertools::Itertools;
+
+fn is_safe(levels: &Vec<usize>) -> bool {
+    levels.iter().tuple_windows().all(|(&l1, &l2)| l1.abs_diff(l2) <= 3) &&
+        (levels.iter().tuple_windows().all(|(l1, l2)| l1 < l2) ||
+         levels.iter().tuple_windows().all(|(l1, l2)| l1 > l2))
+}
+
+fn main() {
+    let safe: usize = io::stdin().lines()
+        .map(|line| line.expect("read error"))
+        .map(|line| {
+            line.split(' ')
+                .map(|level| level.parse::<usize>().expect("not a number"))
+                .collect::<Vec<_>>()
+        })
+        .filter(|levels|{
+            // Brute force solutions for better future
+            (0..levels.len()).any(|i| {
+                let mut levels = levels.to_vec();
+                levels.remove(i);
+                is_safe(&levels)
+            })
+        })
+        .count();
+
+    println!("{safe}")
+}
